25ef0505431fc52b318c5a2cdf9c38583f6fe836
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-03-22 18:48:20 +0100

    caching eksempel kører med data fra get metode - 2

--- src/app/shared/playgrounddata.service.ts
+++ src/app/shared/playgrounddata.service.ts
@@ -15,20 +15,15 @@ export class PlaygrounddataService {
   // path = 'http://localhost:8088/rest/playgrounds';
   path = 'http://130.225.170.204:8088/rest/playground_list';
   playgrounds: any;
-  statusUpdated = new EventEmitter<string>();
 
   constructor(private http: HttpClient) {
-  }
-
-  getPlaygrounds() {
-    this.http.get<IPlayground[]>(this.path)
+    this.playgrounds = this.http.get<IPlayground[]>(this.path)
       .subscribe((response => {
         this.playgrounds = response;
-        this.playgrounds.forEach(play => {
-          console.log(play);
+        this.playgrounds.forEach(playground => {
+          console.log(playground);
         });
       }));
-    return this.playgrounds;
     this.playgrounds.subscribe(next => {
       localStorage[CACHE_KEY] = JSON.stringify(next);
     });
@@ -37,52 +32,4 @@ export class PlaygrounddataService {
       startWith((JSON.parse(localStorage[CACHE_KEY] || '[]')))
     );
   }
-
-  /*getPlaygrounds() {
-    this.isFetching = true;
-    this.http
-      .get(this.path)
-      .pipe(
-        map(response => {
-          const playgroundArray = [];
-          for (const key in response) {
-            if (response.hasOwnProperty(key)) {
-              playgroundArray.push({...response[key], id: key});
-            }
-          }
-          this.playgrounds = playgroundArray;
-        })
-      )
-      .subscribe(playground => {
-        console.log(playground);
-        this.isFetching = false;
-      });
-  }*/
-
-
-  /*  private getHighScores() {
-    console.log('fetching highscores...');
-    this.httpClient.get<[]>(
-      this.path + 'galgeleg/highscore')
-      .subscribe(
-        response => {
-          response.forEach((s) => {
-            this.highScoreList.push(JSON.parse(s));
-          });
-        },
-        err => console.log(err));
-  }*/
-  /*
-    addKinderGarden(newGarden: PlaygroundInterface) {
-      this.kinderGardens.push(newGarden);
-    }*/
-  /*  events1: string[] = [
-      'klatring',
-      'fodbold'
-    ];
-    events2: string[] = [
-      'vandkamp',
-      'stafetløb'
-    ];
-  */
 }
